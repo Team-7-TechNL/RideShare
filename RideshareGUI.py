75ae486ae463190cf8f7add8c3369c1fbde73d90
CarsonFoley <Carson.Foley@keyin.com>
2022-08-25 20:10:11 -0230

    Initial Commit

--- RideshareGUI.py
+++ RideshareGUI.py
@@ -1,18 +1,30 @@
 from PyQt5.QtCore import *
-from PyQt5.QtWidgets import *
+from PyQt5 import QtWidgets
+
 from PyQt5.QtWebEngineWidgets import *
 from geopy.geocoders import Nominatim
 import sys, math
 
-import Distchecker
+
 from main import Mapmain
 Mapmain((0,0))
 
-class MainWindow(QMainWindow):
+import sys
+from PyQt5.QtWidgets import *
 
+
+class MyWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
-        super(MainWindow, self).__init__(*args, **kwargs)
+        super(MyWindow, self).__init__(*args, **kwargs)
+        self.initUI()
 
+    def button_clicked(self):
+        self.label = QLabel("Please select a location")
+        self.label.setStyleSheet("border: 1px solid black")
+        self.label.move(1100, 900)
+        self.label.adjustSize()
+
+    def initUI(self):
         self.browser = QWebEngineView()
         self.browser.setUrl(QUrl("file:///aee.html"))
 
@@ -33,21 +45,47 @@ class MainWindow(QMainWindow):
         self.button.setToolTip('sets your location')
         self.button.move(50, 940)
         self.button.clicked.connect(self.on_click)
-        self.show()
+
+        info = "Please select your role"
+
+        self.label = QLabel(info, self)
+        self.label.setStyleSheet("border: 1px solid black")
+        self.label.setGeometry(875, 900, 120, 20)
+
+
+        self.button1 = QtWidgets.QPushButton("Driver", self)
+        self.button1.setStyleSheet("background-color: white")
+        self.button1.move(1350, 940)
+        self.button1.setGeometry(850, 940, 55, 55)
+        self.button1.clicked.connect(self.button_clicked)
+
+        self.button2 = QPushButton("Passenger", self)
+        self.button2.setStyleSheet("background-color: white")
+        self.button2.move(1450, 940)
+        self.button2.setGeometry(950, 940, 55, 55)
+
+    def update(self):
+        self.label.adjustSize()
+
 
     def on_click(self):
-        try:
-            geolocator = Nominatim(user_agent="RideShareApp")
-            location = geolocator.geocode(self.textbox.text())
-            cords = location.latitude, location.longitude
-            Mapmain(cords)
-            self.browser.reload()
-        except:
-            print("Error")
+        from Distchecker import distchecker
+        geolocator = Nominatim(user_agent="RideShareApp")
+        location = geolocator.geocode(self.textbox.text())
+        cords = location.latitude, location.longitude
+        Mapmain(cords)
+        distchecker(cords)
+        self.browser.reload()
+
+
+
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     screen = app.primaryScreen()
     size = screen.size()
-    window = MainWindow()
+    window = MyWindow()
+    window.show()
     app.exec_()
+
--- main.py
+++ main.py
@@ -1,31 +1,44 @@
+from typing import Tuple
 import folium
-from folium import plugins
+
+def Mapmain(cords):
+
+    #sets up the map to 0,0
+    if cords != (0,0):
+        StartPos = cords
+        Zoom = 19.3
+    else:
+        Zoom = 3
+        StartPos = (0, 0)
 
 
 
-#Start Position of the map
-StartPos = ["23","23"]
+    # Cords could be stored in a DataBase
+    cords = [(47.51044931589316, -52.92471902671029),
+             (47.50997182304524, -52.924989780304585),
+             (47.5117538621506, -52.92535888029291),
+             (47.51239826716989, -52.92605581737727),
+             (47.5119085248456, -52.92771487525117),
+             (47.51798725653911, -52.933987077831056),
+             (47.52029535370516, -52.93654879275768)]
+
+    # Creates the map
+    my_map = folium.Map(location=StartPos, zoom_start=Zoom,min_zoom=0,max_zoom=19.3)
+
+    from Distchecker import distchecker, closetsdriver
+    if closetsdriver != 0:
+        folium.CircleMarker(location=closetsdriver, radius=50, popup="Closet Driver", color="#3186cc",
+                            fill=True,
+                            fill_color="#3186cc", ).add_to(my_map)
+
+
+    # Adds markers to the map
+    for i in cords:
+        icon = folium.Icon(color='white', icon='car', icon_color="black", prefix='fa')
+        folium.Marker(i, icon=icon).add_to(my_map)
 
 
 
-# Cords could be stored in a DataBase
-cords = [(47.51044931589316, -52.92471902671029),
-         (47.50997182304524, -52.924989780304585),
-         (47.5117538621506, -52.92535888029291),
-         (47.51239826716989, -52.92605581737727),
-         (47.5119085248456, -52.92771487525117),
-         (47.51798725653911, -52.933987077831056),
-         (47.52029535370516, -52.93654879275768)]
+    # Displays the map
 
-# Creates the map
-my_map = folium.Map(location=StartPos, zoom_start=13, min_zoom=0, max_zoom=22)
-
-#plugins.Geocoder(position='topleft').add_to(my_map)
-#plugins.LocateControl().add_to(my_map)
-# Adds markers to the map
-for i in cords:
-    icon = folium.Icon(color='white', icon='car', icon_color="black", prefix='fa')
-    folium.Marker(i, icon=icon).add_to(my_map)
-
-# Displays the map
-my_map.save('aee.html')
+    my_map.save('aee.html')
